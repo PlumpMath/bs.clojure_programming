5ec2ccd4db2cab86373e38150889df685aaef7a3
pyoung@archbang <netpyoung@gmail.com>
2013-06-07 15:04:41 +0900

    cleanup

--- clojure_programming/project.clj
+++ clojure_programming/project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [enlive "1.0.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.1"]]
+  )
